10e8d17bd541004c52741fe7e5f588d78675e03e
Ignacio Femenia <femeniax@gmail.com>
2022-07-04 19:35:13 -0300

    Class Methods

--- index.js
+++ index.js
@@ -2,25 +2,44 @@ class Cliente
 {
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
     rutCliente;
 }
+class CuentaCorriente{
+    numero;
+    saldo;
+    agencia;
+
+    depositoEncuenta(valor) {
+        CuentaCorriente2.saldo = CuentaCorriente2.saldo + 100 +valor;
+    }
+}
+
+
 const cliente1 = new Cliente();
-
-
 cliente1.nombreCliente = "Jose";
 cliente1.dniCliente = "12341";
-cliente1.numeroCuenta = "10923";
-cliente1.saldoCuenta = 2000;
+cliente1.rutCliente = "V21422142141"
+
+const CuentaCorriente1 = new CuentaCorriente();
+CuentaCorriente1.numero = "10923";
+CuentaCorriente1.saldo = 2000;
+CuentaCorriente1.agencia = 1011;
+
+
 
 const cliente2 = new Cliente();
-
 cliente2.nombreCliente2 = "Leonardo";
 cliente2.dniCliente2 = "13804060";
-cliente2.numeroCuenta2 = "12345125125";
-cliente2.saldoCuenta2 = 1000;
+cliente2.rutCliente = "V21422141"
 
 
+const CuentaCorriente2 = new CuentaCorriente();
+CuentaCorriente2.numero = "12345125125";
+CuentaCorriente2.saldo = 1000;
+CuentaCorriente2.agencia = 1002;
 console.log(cliente1);
 console.log(cliente2);
+
+console.log(CuentaCorriente2.saldo);
+
+CuentaCorriente2.depositoEncuenta(100);
