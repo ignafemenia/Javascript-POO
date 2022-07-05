1b772055b8d8927cab15ba32bd65a8f1eff6da06
Ignacio Femenia <femeniax@gmail.com>
2022-07-05 18:27:21 -0300

    Create Constructor

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ class CuentaCorriente{
     saldo;
     agencia;
 
-    depositoEncuenta(valor) {
-        CuentaCorriente2.saldo = CuentaCorriente2.saldo + 100 +valor;
+    constructor() {
+        this.#saldo = 0;
+        this.numero = '';
+        this.agencia = '';
+    }
+
+    depositoEnCuenta(valor) {
+        if (valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarDeCuenta(valor) {
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo() {
+        return this.#saldo;
     }
 }
 
