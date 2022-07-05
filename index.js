ee93ca088af00977961a29f11138c8e5d77342ce
Ignacio Femenia <femeniax@gmail.com>
2022-07-05 18:27:59 -0300

    Console log Commits

--- index.js
+++ index.js
@@ -33,31 +33,16 @@ class CuentaCorriente{
 }
 
 
-const cliente1 = new Cliente();
-cliente1.nombreCliente = "Jose";
-cliente1.dniCliente = "12341";
-cliente1.rutCliente = "V21422142141"
+cuentaDeLeonardo = new CuentaCorriente();
+//cuentaDeLeonardo.#saldo = 10;
+let saldo = cuentaDeLeonardo.verSaldo(); 
+console.log('El Saldo actual es '+saldo);
 
-const CuentaCorriente1 = new CuentaCorriente();
-CuentaCorriente1.numero = "10923";
-CuentaCorriente1.saldo = 2000;
-CuentaCorriente1.agencia = 1011;
+saldo = cuentaDeLeonardo.depositoEnCuenta(100);
+console.log('El Saldo actual es '+saldo);
 
+saldo = cuentaDeLeonardo.retirarDeCuenta(100);
+console.log('El Saldo actual es '+saldo);
 
-
-const cliente2 = new Cliente();
-cliente2.nombreCliente2 = "Leonardo";
-cliente2.dniCliente2 = "13804060";
-cliente2.rutCliente = "V21422141"
-
-
-const CuentaCorriente2 = new CuentaCorriente();
-CuentaCorriente2.numero = "12345125125";
-CuentaCorriente2.saldo = 1000;
-CuentaCorriente2.agencia = 1002;
-console.log(cliente1);
-console.log(cliente2);
-
-console.log(CuentaCorriente2.saldo);
-
-CuentaCorriente2.depositoEncuenta(100);
+saldo = cuentaDeLeonardo.depositoEnCuenta(10)
+console.log('El Saldo actual es '+saldo);
